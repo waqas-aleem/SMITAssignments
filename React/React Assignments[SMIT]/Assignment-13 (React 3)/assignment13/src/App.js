ff5d9296b7c9539ea6aec7429ce3213efd0a5ca9
waqas-aleem <waqas.aleem1990@gmail.com>
2022-02-15 11:47:20 +0500

    Uploaded Assignment - 13

--- React/React Assignments[SMIT]/Assignment-13 (React 3)/assignment13/src/App.js
+++ React/React Assignments[SMIT]/Assignment-13 (React 3)/assignment13/src/App.js
@@ -79,14 +79,14 @@ function App() {
             <p className="text-secondary">January 1,2014 by <a href="" className="text-decoration-none">Mark</a></p>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. when an unknown printer took a galley of type and scrambled.  </p>
 
-            <hr className="mt-5"/>
+            <hr className="mt-3"/>
 
  
             <p>Lorem Ipsum is simply dummy text of the <a href="" className="card-link text-decoration-none">printing and typesetting</a> industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
 
-           <p className="mt-5">Lorem Ipsum is simply <b>dummy text of the printing and</b> typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took.</p>
+           <p className="mt-3">Lorem Ipsum is simply <b>dummy text of the printing and</b> typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took.</p>
 
-           <p className="mt-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took.</p>
+           <p className="mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took.</p>
            <h1 className="mt-5">Heading</h1>
            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
 
@@ -97,14 +97,14 @@ function App() {
 
            <h1>Sub-heading</h1>
            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets.</p>
-           <ul className="mt-3 mb-4">
+           <ul className="mt-3 mb-4 ms-5">
              <li>Lorem Ipsum is simply dummy text of the printing</li>
              <li>Lorem Ipsum is simply dummy text of the printing</li>
              <li>Lorem Ipsum is simply dummy text of the printing</li>
            </ul>
            
             <p className="mt-3 mb-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-            <ol>
+            <ol className="ms-5">
               <li>Lorem Ipsum is simply dummy text of the printing and typesetting</li>
               <li>Lorem Ipsum is simply dummy text of the printing and typesetting</li>
               <li>Lorem Ipsum is simply dummy text of the printing and typesetting</li>
@@ -112,15 +112,63 @@ function App() {
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
 
             <h1 className="mt-5">Another blog post</h1>
+            <p className="text-secondary">December 23, 2013 by <a href="#" className="card-link text-decoration-none">Jacob</a></p>
+              <p>Lorem Ipsum is simply dummy text of the <a href="#" className="card-link text-decoration-none">printing and typesetting </a>industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+              <p>Lorem Ipsum is simply dummy <b>text of the printing and typesetting industry.</b> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled.</p>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled.</p>
+
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
+
+            <h1 className="mt-5">New feature</h1>
             <p className="text-secondary">December 14, 2013 by <a href="#" className="card-link text-decoration-none">Chris</a></p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets.</p>
+
+            <ul className="mt-3 ms-5">
+              <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+              <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+              <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+            </ul>
+          <p className="mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled.</p>
+          <p className="mt-3 mb-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+        <div className="mb-5">
+          <button type="button" className="btn btn-outline-primary me-1 rounded-pill">Older</button>
+          <button type="button" className="btn btn-outline-secondary rounded-pill">Newer</button>
+        </div>
+          
           </div>
           {/* Aside bar */}
           <div className="col-lg-3 ps-3 pe-0">
           <div className="abouttext py-4 px-3">
             <h4 className='fw-bolder fst-italic'>About</h4>
-            <p className="abouttext2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+            <p className="abouttext2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,Contrary to popular belief.</p>
           </div>
+          
+            <div className="mt-4">
+            <h3 className="fst-italic">Archives</h3>
+            <a href="#" className="card-link text-decoration-none">March 2014</a><br/>
+            <a href="#" className="card-link text-decoration-none">Feburary 2014</a><br/>
+            <a href="#" className="card-link text-decoration-none">January 2014</a><br/>
+            <a href="#" className="card-link text-decoration-none">December 2013</a><br/>
+            <a href="#" className="card-link text-decoration-none">November 2013</a><br/>
+            <a href="#" className="card-link text-decoration-none">October 2013</a><br/>
+            <a href="#" className="card-link text-decoration-none">September 2013</a><br/>
+            <a href="#" className="card-link text-decoration-none">August 2013</a><br/>
+            <a href="#" className="card-link text-decoration-none">July 2013</a><br/>
+            <a href="#" className="card-link text-decoration-none">June 2013</a><br/>
+            <a href="#" className="card-link text-decoration-none">May 2013</a><br/>
+            <a href="#" className="card-link text-decoration-none">April 2013</a>
+            </div>
 
+
+            <h3 className="fst-italic mt-5">Elsewhere</h3>
+            <div className="mt-2">
+            <a href="#" className="card-link text-decoration-none">GitHub</a><br/>
+            <a href="#" className="card-link text-decoration-none">Twitter</a><br/>
+            <a href="#" className="card-link text-decoration-none">Facebook</a><br/>
+      
+            </div>
+           
             
           </div>
         </div>
