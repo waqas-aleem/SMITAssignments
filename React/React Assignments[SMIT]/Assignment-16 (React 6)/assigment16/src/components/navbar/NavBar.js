bedf913b7e0089c9cb1f30925fab0a2153a9cac6
waqas-aleem <waqas.aleem1990@gmail.com>
2022-03-12 11:02:01 +0500

    Upload

---
+++ React/React Assignments[SMIT]/Assignment-16 (React 6)/assigment16/src/components/navbar/NavBar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light navbarbg">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"><button style={{ border: "none", backgroundColor: "#0078D7", color: "white" }}>
+                        <i className='bx bxs-grid' ></i> <span className='mx-md-3'>To Do</span></button></a>
+
+                    <form className="search m-auto">
+                        <input className="form-control me-2" type="search" aria-label="Search" />
+                        <i class="fal fa-search"></i>
+
+                    </form>
+                    <div className='mx-md-3'>
+                        <button style={{
+                            border: "none", backgroundColor: "#0078D7", color: "white",
+                            fontSize: "20px", height: "100%"
+                        }}><i class="fal fa-cog"></i></button>
+                    </div>
+                    <div className='mx-md-3'>
+                        <button style={{
+                            border: "none", backgroundColor: "#0078D7", color: "white",
+                            fontSize: "20px", height: "100%"
+                        }}>
+                            <i class='bx bx-question-mark' ></i>
+                        </button>
+                    </div>
+                    <div className='mx-md-3'>
+                        <button style={{
+                            border: "none", backgroundColor: "#0078D7", color: "white",
+                            fontSize: "20px", height: "100%"
+                        }}>
+                            <i class="fal fa-megaphone"></i>
+                            <span class="translate-middle badge rounded-pill bg-dark">
+                                3
+                                <span class="visually-hidden">unread messages</span>
+                            </span>
+                        </button>
+                    </div>
+
+                    <button type='button' className='profilebtn'> RS </button>
+
+                </div>
+            </nav>
+
+        </div>
+    )
+}
